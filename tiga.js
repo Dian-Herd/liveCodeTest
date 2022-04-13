380f949bec3bba1fea543d2d02261ec13440ad71
Dian-Herd <dian92943@gmail.com>
2022-04-13 15:52:09 +0700

    revisi 1 nomor 3

--- tiga.js
+++ tiga.js
@@ -1,10 +1,20 @@
 const printDigitValue = (value) => {
-    let array = value.split('');
-    for(let i = 0; i < array.length; i++){
-        if(!isNaN(array[i])){
-            console.log(array[i]);
-        }
+  let array = value.split("");
+  let hasil = "";
+  let hasildigit = "";
+  for (let i = 0; i < array.length; i++) {
+    if (!isNaN(array[i])) {
+      hasil += array[i];
     }
-}
+  }
+  console.log(hasil);
+  for (let i = 0; i < hasil.length; i++) {
+    for (let j = hasil.length - 1; j > i; j--) {
+      hasildigit += "0";
+    }
+    console.log(hasil[i] + hasildigit);
+    hasildigit = "";
+  }
+};
 
-printDigitValue('9.86-A5.321');
+printDigitValue("9.86-A5.321");
